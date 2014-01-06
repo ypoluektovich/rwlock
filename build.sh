669b1f163a448c6ba6512099ad1b978963aa448a
Yanus Poluektovich <ypoluektovich@gmail.com>
2014-01-06 13:33:46 +0400

    initial commit (build script)

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+if [[ ! -e lib ]]; then
+    mkdir lib
+elif [[ ! -d lib ]]; then
+    echo "Error: lib is not a directory!" 1>&2
+    exit 1
+fi
+cd lib
+
+LIBNAME=rwlock
+LIBVER_MAJOR=1
+LIBVER_MINOR=0
+LIBVER_REV=0
+
+SONAME="lib${LIBNAME}.so.${LIBVER_MAJOR}"
+g++ -std=c++11 -fpic -Wall -g -c -I ../include ../src/*.cpp
+g++ -g -shared "-Wl,-soname,${SONAME}" -o "${SONAME}.${LIBVER_MINOR}.${LIBVER_REV}" *.o
